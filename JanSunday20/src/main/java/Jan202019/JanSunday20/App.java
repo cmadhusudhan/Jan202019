4f71ee15d394c6ada90287afbdb7eac15eda07be
mchinnala <mchinnala@SHVLSP2012F1L.SLS.ads.valuelabs.net>
2019-01-20 14:48:25 +0530

    Added Jan202019

---
+++ JanSunday20/src/main/java/Jan202019/JanSunday20/App.java
@@ -0,0 +1,13 @@
+package Jan202019.JanSunday20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World Java!" );
+    }
+}
